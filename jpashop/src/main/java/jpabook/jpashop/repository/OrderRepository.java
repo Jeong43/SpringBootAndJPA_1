363d68ddb094d3b4afa3fe1d7e5f9a25053f0f6b
mi-hwa <43jmih@naver.com>
2023-07-27 09:23:13 +0900

    주문 도메인 개발: queryDSL 적용

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,17 @@
 package jpabook.jpashop.repository;
 
+import static jpabook.jpashop.domain.QMember.*;
+import static jpabook.jpashop.domain.QOrder.*;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import javax.persistence.EntityManager;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,7 +28,30 @@ public class OrderRepository {
   }
 
   public List<Order> findAll(OrderSearch orderSearch) {
-    return null;
+    //TODO private final JPAQueryFactory query; 로 선언 후 생성자 주입으로 개선
+    JPAQueryFactory query = new JPAQueryFactory(em);
+
+    return query
+        .select(order)
+        .from(order)
+        .join(order.member, member)
+        .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+        .limit(1000)
+        .fetch();
+  }
+
+  private BooleanExpression statusEq(OrderStatus statusCond) {
+    if (statusCond == null) {
+      return null;
+    }
+    return order.status.eq(statusCond);
+  }
+
+  private static BooleanExpression nameLike(String memberName) {
+    if (!StringUtils.hasText(memberName)) {
+      return null;
+    }
+    return member.name.like(memberName);
   }
 
 }
